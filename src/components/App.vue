459574d1378fa81dcfe5b3fc74778168caeb3895
Mateusz Rorat <mateusz.rorat@contractors.roche.com>
2018-08-31 12:48:25 +0200

    initial-setup

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'app',
+      data () {
+        return {
+          msg: 'ASTEK example'
+        }
+      }
+    }
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983
+  }
+</style>
